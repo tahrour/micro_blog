5bc90e11891886b94e982fab80ac8cfc21026f8d
tahrour <d.tahrour@gmail.com>
2017-01-14 15:33:25 +0100

    modification/suppression/connexion/pagination

--- index.php
+++ index.php
@@ -19,6 +19,10 @@ $query = 'SELECT * FROM messages where id='.$_GET['id'];
 }
 ?>
 
+<?php
+if ($connecter==true)
+{
+?>
 <div class="row">              
     <form method="post"  action="message.php">
         <div class="col-sm-10">  
@@ -32,24 +36,41 @@ $query = 'SELECT * FROM messages where id='.$_GET['id'];
         </div> 
 		</form>
 		</div>
-
-
+		<?php
+          }
+?>
 
 
 
 
 
 <?php
-$query = 'SELECT * FROM messages';
-$stmt = $pdo->query($query);
 
+$page = (!empty($_GET['page']) ? $_GET['page'] : 1);
+$limite = 2;
+$debut = ($page - 1) * $limite;
+$query = 'SELECT count(*) as nb FROM messages ';
+    $stmt = $pdo->query($query);
+    while( $data=$stmt->fetch())
+    {
+        $count=$data['nb'];
+    }
+$nbpage= $count/$limite;    
+$query = 'SELECT * FROM messages ORDER BY date DESC LIMIT :limite OFFSET :debut ';
+$stmt = $pdo->prepare($query);
+$stmt->bindValue(':limite',$limite,PDO::PARAM_INT);
+$stmt->bindValue('debut', $debut, PDO::PARAM_INT);
+$stmt->execute();
 while ($data = $stmt->fetch()) {
 	?>
 	<blockquote>
 		<?= $data['contenu'] ?>
 	</blockquote>
 			<div class="row"> 		
-
+<?php
+if ($connecter==true)
+{
+?>
 <div class="col-sm-2">
             <a href="supprimer.php?id=<?=$data['id']?>" > Supprimer</a>
 			
@@ -62,7 +83,9 @@ while ($data = $stmt->fetch()) {
              <a href="index.php?id=<?=$data['id']?>" > Modifier</a>
 
         </div>  
-
+<?php
+}
+?>
 		<div class="col-sm-2">
 			<?= date('d/m/Y H:i:s',$data['date']) ?>
 		</div>
@@ -72,5 +95,31 @@ while ($data = $stmt->fetch()) {
 <?php
 }
 ?>
+<?php   
+    if ($page > 1):
+    ?> 
+  
+      <a class="page-link" href="?page=<?php echo $page - 1; ?>" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+     
+      </a> <?php
+endif;
+for ($i = 1; $i <= $nbpage+1; $i++):
+    ?>
+    <a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+    <?php
+endfor;
+if ($page < $nbpage):
+    ?>
+  
+      <a class="page-link" href="?page=<?php echo $page + 1; ?>" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+        
+      </a>
+ <?php
+endif;
+?>
+
+
 
 <?php include('includes/bas.inc.php'); ?>
--- page_de_connexion.php
+++ page_de_connexion.php
@@ -2,28 +2,49 @@
 	include('includes/connexion.inc.php');
 	?>
 	<?php
+// pour détruire complètement la session, on efface également
+// le cookie de session.
+
+   setcookie("sessioncookies",'', time() +500);
+
+
 // on se connecte à MySQL 
 $db = mysql_connect('localhost', 'root'); 
 mysql_select_db('micro_blog',$db); 
-
 if(isset($_POST) && !empty($_POST['login']) && !empty($_POST['password'])) {
-$_POST['password'] =md5($_POST['password']);
+$login=$_POST['login'];
+$password=md5($_POST['password']);
   // on recupére le password de la table qui correspond au login du visiteur
   $sql = "select mdp from utilisateur where email='".$login."'";
   $req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
-
   $data = mysql_fetch_assoc($req);
-
   if($data['mdp'] !=$password) {
     echo '<div class="alert alert-dismissable alert-danger">
   <button type="button" class="close" data-dismiss="alert">x</button>
   <strong>Oh Non !</strong> Mauvais login / password. Merci de recommencer !
-</div>'.$password;
+</div>';
   }
   
   else {
     session_start();
     $_SESSION['login'] = $login;
+	session_destroy();  
+	$query = "SELECT * FROM utilisateur where email='$login' && mdp='$password'";
+$stmt = $pdo->query($query);
+if($data=$stmt->fetch())
+{
+    $id=$data['id'];
+$sid=md5($_POST['login'].$_POST['password'].time());
+setcookie("sessioncookies",$sid);
+
+$query = 'UPDATE  utilisateur SET sid=:sid where id= :id';
+    $prep = $pdo->prepare($query);
+    $prep->bindValue(':sid',$sid);
+    $prep->bindValue(':id', $id);
+    $prep->execute();
+	
+}
+    echo" Vous êtes  déconnecté"; 
     
     echo '<div class="alert alert-dismissable alert-success">
   <button type="button" class="close" data-dismiss="alert">×</button>
